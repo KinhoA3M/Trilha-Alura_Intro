60d2dd6ed74ed6c9b854334c68b019cef0475a16
KinhoA3M <erick.a.menezes31@gmail.com>
2021-03-15 15:23:56 -0300

    Commit final cap 4

--- Curso Alura 1/MyPlayground.playground/Contents.swift
+++ Curso Alura 1/MyPlayground.playground/Contents.swift
@@ -1,41 +1,52 @@
 import UIKit
 
-// Criar a Primeira Refeição
-
-let nome: String = "Macarrão"
-let felicidade: String = "5"
-
-//Criar Segunda Refeição
-
-let nome2:  String = "Churros"
-let felicidade2: String = "4"
-
-//----------------
-
-// Criar Classe que agrupe as Características
-
 class Refeicao {
-    var nome: String = "Macarrão"
-    let felicidade: String = "5"
+    var nome: String?
+    var felicidade: String?
 }
 
 //Instanciando a classe
 
-let refeicao: Refeicao = Refeicao()
+let refeicao = Refeicao()
+refeicao.nome = "Macarrao"
 
-refeicao.nome = "Arroz"
+ //Cuidado => forced unwrap
+//print(refeicao.nome!)
 
-refeicao.nome = "Feijão"
-
-print(refeicao.nome)
-
-class Refeicao2 {
-    var nome: String = "Churros"
-    var felicidade: String = "4"
+if refeicao.nome != nil {
+    print(refeicao.nome!)
 }
 
-var refeicao2 = Refeicao2()
+//Boas práticas para extrair valores opcionais:
 
-print(refeicao2.nome)
+if let nome = refeicao.nome {
+    print(nome)
+}
+
+//guard let (pode chamar variavel de fora da condicional
+
+func exibeNomeDaRefeicao() {
+    if let nome = refeicao.nome {
+        print(nome)
+    }
+    
+    guard let nome = refeicao.nome else {
+        print("Sem Valor")
+        return
+    }
+    
+    print(nome)
+}
+
+exibeNomeDaRefeicao()
+
+let numero = Int("5")
+
+if let n = numero {
+    print(n)
+} else {
+    print("Erro ao converter para int")
+}
+
 
 
