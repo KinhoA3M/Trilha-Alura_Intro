fa783ce9a3b59cf6bdc431a2ee95eccb4f1d1ffb
KinhoA3M <erick.a.menezes31@gmail.com>
2021-03-16 09:48:12 -0300

    Commit módulo 6 curso intro 2

--- Curso Alura 1/Controller/ViewController.swift
+++ Curso Alura 1/Controller/ViewController.swift
@@ -11,16 +11,62 @@ protocol AdicionaRefeicaoDelegate {
     func add(_ refeicao: Refeicao)
 }
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+        
     //MARK: - Atributos
     
     var delegate: AdicionaRefeicaoDelegate?
+    //var itens: [String] = ["Molho de tomate", "Queijo", "Molho Apimentado", "Manjericao"]
     
+    var itens: [Item] = [Item(nome: "Molho de Tomate", calorias: 40.0),
+                         Item(nome: "Queijo", calorias: 40.0),
+                         Item(nome: "Molho Apimentado", calorias: 40.0),
+                         Item(nome: "Manjericao", calorias: 40.0)]
+    
+    var itensSelecionados: [Item] = []
+                         
     // MARK: - IBOutlets
 
     @IBOutlet var nomeTextField: UITextField!
     @IBOutlet weak var felicidadeTextField: UITextField!
+    
+    // MARK: -UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itens.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        let linhaDaTabela = indexPath.row
+        let item = itens[linhaDaTabela]
+        
+        celula.textLabel?.text = item.nome
+        
+        return celula
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let celula = tableView.cellForRow(at: indexPath) else {return}
+        
+        if celula.accessoryType == .none {
+            celula.accessoryType = .checkmark
+            
+            let linhaDaTabela = indexPath.row
+            itensSelecionados.append(itens[linhaDaTabela])
+        } else {
+            celula.accessoryType = .none
+            
+            let item  = itens[indexPath.row]
+            if let position = itensSelecionados.index(of: item) {
+                itensSelecionados.remove(at: position)
+            }
+        }
+    }
     
     // MARK: - IBActions
 
@@ -34,7 +80,8 @@ class ViewController: UIViewController {
             return
         }
 
-        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade, itens: itensSelecionados)
+        refeicao.itens = itensSelecionados
 
         print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
         
