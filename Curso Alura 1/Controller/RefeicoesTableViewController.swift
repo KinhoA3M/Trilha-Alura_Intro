5a593a342ccf27af71c4e8c441da7b3b2d88e7c6
KinhoA3M <erick.a.menezes31@gmail.com>
2021-03-16 01:45:02 -0300

    Commit Módulo 2 cap 06

---
+++ Curso Alura 1/Controller/RefeicoesTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RefeicoesTableViewController.swift
+//  Curso Alura 1
+//
+//  Created by Erick Avila Menezes on 15/03/21.
+//
+
+import UIKit
+
+class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDelegate {
+    
+    var refeicoes = [Refeicao(nome: "Macarrao", felicidade: 4),
+                     Refeicao(nome: "Pizza", felicidade: 4),
+                     Refeicao(nome: "Comida Japonesa", felicidade: 5)]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return refeicoes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let celula = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let refeicao = refeicoes[indexPath.row]
+        celula.textLabel?.text = refeicao.nome
+
+        return celula
+    }
+    
+    func add(_ refeicao: Refeicao) {
+        refeicoes.append(refeicao)
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "adicionar" {
+            if let viewController = segue.destination as? ViewController {
+                viewController.delegate = self
+            }
+        }
+    }
+}
--- Curso Alura 1/Controller/ViewController.swift
+++ Curso Alura 1/Controller/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
+protocol AdicionaRefeicaoDelegate {
+    func add(_ refeicao: Refeicao)
+}
+
 class ViewController: UIViewController {
+    
+    //MARK: - Atributos
+    
+    var delegate: AdicionaRefeicaoDelegate?
+    
+    // MARK: - IBOutlets
 
     @IBOutlet var nomeTextField: UITextField!
     @IBOutlet weak var felicidadeTextField: UITextField!
+    
+    // MARK: - IBActions
 
     @IBAction func adicionar(_ sender: Any) {
-
+        
         guard let nomeDaRefeicao = nomeTextField?.text else {
             return
         }
@@ -25,5 +37,8 @@ class ViewController: UIViewController {
         let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
 
         print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+        
+        delegate?.add(refeicao)
+        navigationController?.popViewController(animated: true)
     }
 }
